ad96a51a79083fc12b8e722e989c46e19bcbf8ab
alanhe <qianghe421@gmail.com>
2022-02-25 23:17:43 +0800

    自动更新脚本下载地址

---
+++ .github/workflows/update-url.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+function main() {
+  const targetFolder = path.resolve(__dirname, '../../');
+  const files = fs.readdirSync(targetFolder);
+  const url = [];
+  files.forEach(item => {
+    if (item.match(/\.js$/)) {
+      url.push(`${item}`);
+    }
+  });
+  const baseRawUrl = 'https://raw.githubusercontent.com/alanhg/tampermonkey-scripts/main/';
+  const urlListStr = url.map(item => `- [${item}](${baseRawUrl}${item})`).join('\n');
+  const readmeFile = targetFolder + '/README.md';
+  let readmeContent = fs.readFileSync(readmeFile, 'utf8');
+  const newReadmeContent = readmeContent.replace(/(?<=<!-- start -->)(\s|\S)+(?=<!-- end -->)/, '\n' + urlListStr + '\n')
+  fs.writeFileSync(readmeFile, newReadmeContent);
+}
+
+main();
