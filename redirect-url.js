852c7771e1ac1e2ab3340e71f1ba2a486b2d6dfe
Alan.He <qianghe421@gmail.com>
2022-02-26 22:32:33 +0800

    Update redirect-url.js

--- redirect-url.js
+++ redirect-url.js
@@ -2,7 +2,7 @@
 // @name         Redirect URL
 // @namespace    https://1991421.cn
 // @version      0.1
-// @description  è§£å³é¨åç«ç¹è®¿é®çäºæ¬¡æ¦æªï¼èªå¨è·³è½¬å°ç®æ é¡µï¼æ¯å¦å¾®ä¿¡ä¸­é¾æ¥çæ¦æª
+// @description  解决恼人的网页拦截，比如微信打开链接
 // @author       Alan He
 // @match        https://weixin110.qq.com/cgi-bin/mmspamsupport-bin/newredirectconfirmcgi*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -18,7 +18,6 @@
       }
 
     if(window.location.href.match(/^https:\/\/weixin110.qq.com\/cgi-bin\/mmspamsupport-bin\/newredirectconfirmcgi/)){
-      // document.getElementsByClassName('weui-btn weui-btn_default')[0].click();
       window.location.href=htmlDecode(cgiData.url);
     }
 })();
