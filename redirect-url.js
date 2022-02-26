55d569c24ed559f482051dd5f6f38746b586ab33
Alan.He <qianghe421@gmail.com>
2022-02-26 21:55:13 +0800

    Update redirect-url.js

--- redirect-url.js
+++ redirect-url.js
@@ -2,11 +2,12 @@
 // @name         Redirect URL
 // @namespace    https://1991421.cn
 // @version      0.1
-// @description  解决部分站点访问的二次拦截，自动跳转到目标页，比如微信中链接的拦截
+// @description  è§£å³é¨åç«ç¹è®¿é®çäºæ¬¡æ¦æªï¼èªå¨è·³è½¬å°ç®æ é¡µï¼æ¯å¦å¾®ä¿¡ä¸­é¾æ¥çæ¦æª
 // @author       Alan He
 // @match        https://weixin110.qq.com/cgi-bin/mmspamsupport-bin/newredirectconfirmcgi*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @run-at document-end
 // ==/UserScript==
 
 (function() {
